dce521cebf51a8ea5968f08d703282531619d283
Vladyslav Kudriashov <129685708+hexlabelposition@users.noreply.github.com>
2026-08-24 20:25:47 +0200

    test: run identity context with PostgreSQL

--- identity/src/test/java/dev/identity/clud/CludApplicationTests.java
+++ identity/src/test/java/dev/identity/clud/CludApplicationTests.java
@@ -3,7 +3,14 @@ package dev.identity.clud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:tc:postgresql:17-alpine:///clud",
+		"spring.datasource.username=test",
+		"spring.datasource.password=test",
+		"spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"jwt.secret=Y2x1ZC10ZXN0LWp3dC1zZWNyZXQtMzItYnl0ZXMh"
+})
 class CludApplicationTests {
 
 	@Test

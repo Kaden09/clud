c99e044c551dde20bb67e352e8a1087364727ee7
kaden <ketkette7@gmail.com>
2026-08-30 19:59:09 +0400

    feat: add OpenAPI documentation

--- storage/src/main/java/dev/storage/clud/storage/StorageController.java
+++ storage/src/main/java/dev/storage/clud/storage/StorageController.java
@@ -1,5 +1,7 @@
 package dev.storage.clud.storage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -10,22 +12,26 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/files")
+@RequestMapping("/api/storage")
 @RequiredArgsConstructor
+@Tag(name = "Storage", description = "Storage operations")
 public class StorageController {
     private final StorageService service;
 
     @PostMapping("/upload")
+    @Operation(summary = "Upload file")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         return ResponseEntity.ok(service.upload(file));
     }
 
-    @GetMapping
+    @GetMapping("/files")
+    @Operation(summary = "Find all files")
     public ResponseEntity<List<String>> findAllFiles() {
         return ResponseEntity.ok(service.findAll());
     }
 
     @GetMapping("/download/{filename}")
+    @Operation(summary = "Download file")
     public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
@@ -33,6 +39,7 @@ public class StorageController {
     }
 
     @DeleteMapping("/{filename}")
+    @Operation(summary = "Delete file")
     public ResponseEntity<Void> deleteFile(@PathVariable String filename) {
         service.delete(filename);
         return ResponseEntity.ok().build();

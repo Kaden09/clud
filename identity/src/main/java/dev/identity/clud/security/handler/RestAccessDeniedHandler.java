92eee5b633a22a08d433e763ed9412ec11a819e9
Vladyslav Kudriashov <129685708+hexlabelposition@users.noreply.github.com>
2026-08-31 22:44:50 +0200

    fix identity security handler references

--- identity/src/main/java/dev/identity/clud/security/handler/RestAccessDeniedHandler.java
+++ identity/src/main/java/dev/identity/clud/security/handler/RestAccessDeniedHandler.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RestAccessDeniedHandler implements AccessDeniedHandler {
 
-    private final SecurityApiErrorWriter errorWriter;
+    private final SecurityErrorResponseWriter errorWriter;
 
     @Override
     public void handle(
--- identity/src/main/java/dev/identity/clud/security/handler/RestAuthenticationEntryPoint.java
+++ identity/src/main/java/dev/identity/clud/security/handler/RestAuthenticationEntryPoint.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final SecurityApiErrorWriter errorWriter;
+    private final SecurityErrorResponseWriter errorWriter;
 
     @Override
     public void commence(

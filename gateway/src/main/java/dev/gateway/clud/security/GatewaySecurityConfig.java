9f14b4489d8cd03c5796bb507244c807a20d328c
Максим <126879020+Kaden09@users.noreply.github.com>
2026-09-07 21:27:44 +0400

    feat: created metrics for every service (#17)

--- gateway/src/main/java/dev/gateway/clud/security/GatewaySecurityConfig.java
+++ gateway/src/main/java/dev/gateway/clud/security/GatewaySecurityConfig.java
@@ -63,7 +63,8 @@ public class GatewaySecurityConfig {
                                 "/v3/api-docs/**",
                                 "/docs/**",
                                 "/swagger-ui/**",
-                                "/actuator/health/**").permitAll()
+                                "/actuator/health/**",
+                                "/actuator/prometheus/**").permitAll()
                         .requestMatchers("/api/files/internal/**").denyAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
--- identity/src/main/java/dev/identity/clud/security/SecurityConfig.java
+++ identity/src/main/java/dev/identity/clud/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                         .authenticationEntryPoint(authenticationEntryPoint)
                         .accessDeniedHandler(accessDeniedHandler))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**", "/actuator/health/**", "/v3/api-docs/**", "/docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/auth/**", "/actuator/health/**", "/actuator/prometheus/**", "/v3/api-docs/**", "/docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated())
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
